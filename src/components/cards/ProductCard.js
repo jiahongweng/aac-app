ad00781c3704f91374e6e6f0b4313505e389df87
jiahongweng <web.devmastersuccess@gmail.com>
2020-05-14 09:18:22 -0500

    Add lazy loading to product images

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -2,30 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardText,
-  CardImg,
-  Row,
-} from 'reactstrap';
+  LazyLoadImage,
+  trackWindowScroll,
+} from 'react-lazy-load-image-component';
+import { Card, CardBody, CardSubtitle, CardText, Row } from 'reactstrap';
 import { Colxx } from 'components/common/CustomBootstrap';
 import { SS_ACTIVEWARE } from 'utils/constants';
 
-const ProductCard = ({ product, isSelect, onClickItem }) => (
+const ProductCard = ({ product, isSelect, onClickItem, scrollPosition }) => (
   <Colxx xs="6" md="4" xl="3" xxl="2" className="mb-3" key={product.id}>
     <Card
       onClick={(event) => onClickItem(event, product.id)}
-      className={classnames({
+      className={classnames('product-card', {
         active: isSelect,
       })}
     >
-      <div className="position-relative">
-        <CardImg
-          top
+      <div className="card-img-container">
+        <LazyLoadImage
           alt={product.title}
+          className="card-img-top"
+          effect="opacity"
+          scrollPosition={scrollPosition}
+          placeholderSrc={`${
+            SS_ACTIVEWARE.CDN_BASE
+          }/${product.styleImage.replace(`_fm`, `_fs`)}`}
           src={`${SS_ACTIVEWARE.CDN_BASE}/${product.styleImage}`}
-        />
+        ></LazyLoadImage>
       </div>
       <CardBody>
         <Row>
@@ -43,13 +45,15 @@ const ProductCard = ({ product, isSelect, onClickItem }) => (
 
 ProductCard.propTypes = {
   product: PropTypes.object.isRequired,
+  scrollPosition: PropTypes.object,
   isSelect: PropTypes.bool,
   onClickItem: PropTypes.func,
 };
 
 ProductCard.defaultProps = {
+  scrollPosition: null,
   isSelect: false,
   onClickItem: () => {},
 };
 
-export default ProductCard;
+export default trackWindowScroll(ProductCard);
--- src/styles.js
+++ src/styles.js
@@ -2,4 +2,5 @@ import 'assets/css/vendor/bootstrap.min.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import 'react-table/react-table.css';
 import 'react-image-lightbox/style.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import 'assets/css/sass/themes/aac.light.purple.scss';
