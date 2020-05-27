471ccffafcced682fd31d30cf93bd56c1bbbc7e8
jiahongweng <web.devmastersuccess@gmail.com>
2020-05-27 04:53:34 -0500

    Fix aws deploy issue

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@
 # This script is executed after the source is copied to the instances
 cd /var/www/html/aac-app
 
-chown ubuntu:ubuntu -R /var/www/html/aac-app
+source ~/.bashrc
 
-/home/ubuntu/.nvm/versions/node/v12.16.3/bin/npm install
+npm install
