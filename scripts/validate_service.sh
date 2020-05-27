3775d9510849e7ba039538386beee2977d98fa8f
jiahongweng <web.devmastersuccess@gmail.com>
2020-05-27 08:43:25 -0500

    Fix aws deploy issue

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+listenCount=$(netstat -an | grep 3000 | grep LISTEN | wc -l)
 
-# This script is used to validate application 
-ipaddr=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-listencount=$(netstat -an | grep 3000 | grep LISTEN | wc -l)
-if [ "$listencount" -lt 1 ]; then
-    exit 1
+if [ "$listenCount" -lt 1 ]; then
+  exit 1
 else
-    exit 0
+  exit 0
 fi
