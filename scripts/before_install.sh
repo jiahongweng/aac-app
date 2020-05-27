8194c5c2279a984e94e814cabe5525e254ea5d9d
jiahongweng <web.devmastersuccess@gmail.com>
2020-05-27 09:55:00 -0500

    Fix aws deploy issue

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,18 +2,8 @@
 
 # This script is executed before copying the source
 
-yum -y update
+sudo apt-get update
 
-# curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-# yum -y install nodejs
+. /home/ubuntu/.nvm/nvm.sh
 
-npm install -g pm2
 pm2 update
-
-export app_root=/var/www/html/aac-app
-if [ -d "$app_root" ];then
-    rm -rf /var/www/html/aac-app
-    mkdir -p /var/www/html/aac-app
-else
-    mkdir -p /var/www/html/aac-app
-fi
